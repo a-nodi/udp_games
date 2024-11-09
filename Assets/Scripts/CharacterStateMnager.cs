28ada1f1cbab323380e47f552786f7ec1c795483
Yechan Park <pych05@unist.ac.kr>
2024-11-09 20:29:38 +0900

    Fix: comment them all

--- Assets/Scripts/CharacterStateMnager.cs
+++ Assets/Scripts/CharacterStateMnager.cs
@@ -19,17 +19,17 @@ public class CharacterStateMnager : MonoBehaviour
     public bool setState(States state){
         // set the character's state.
         // return true if the state is changed succesfully, otherwise false.
-        switch (state){
-            case == Walking:
-                // write code here
-                // ex1) check state now before changing it if the state can really be changed
-                // ex2) change animation state of the character object.
-                break;
-            case == Running:
-                // ...
-                break;
-            // and so on...
-        }
+        // switch (state){
+        //     case == States.Walking:
+        //         // write code here
+        //         // ex1) check state now before changing it if the state can really be changed
+        //         // ex2) change animation state of the character object.
+        //         break;
+        //     case == States.Running:
+        //         // ...
+        //         break;
+        //     // and so on...
+        // }
         return true;
     }
 }
