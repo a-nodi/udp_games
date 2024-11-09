92b61cc07516a246a5a7b3ba268a55b743a9394b
a-nodi <leeyoonhyuk0@naver.com>
2024-11-09 22:02:17 +0900

    [Feat] Added basic event pipeline

---
+++ Assets/Scripts/EventType.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public enum EVENT_TYPE
+{
+    // Animation Events
+    WALK,
+    RUN,
+    FALL,
+    FOUND,
+    JUMP,
+
+    // Game Events
+    GET_SCORE,
+    GAME_START,
+    GAME_OVER,
+
+    // UI Events
+    UPDATE_SCORE,
+
+
+}
