665a35cb967f198329c8af97961470814b1b6126
Kim-sumin57 <tnals2273@dgist.ac.kr>
2024-11-09 21:19:30 +0900

    Fix

--- Assets/Scripts/background.cs
+++ Assets/Scripts/background.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 
-public class MoveLeftAndLoop : MonoBehaviour
+public class background : MonoBehaviour
 {
     public float speed = 2.0f; 
-    public float screenRightEdge = 9.0f; 
-    public float screenLeftEdge = -9.0f; 
+    public float screenRightEdge = 17.6f;
+    public float screenLeftEdge = -17.6f; 
+
+    void Start()
+    {
+
+        transform.position = new Vector3(0, 0, transform.position.z);
+        transform.localScale = new Vector3(0.75f, 0.6f, transform.localScale.z);
+    }
 
     void Update()
     {
         
         transform.position += Vector3.left * speed * Time.deltaTime;
 
-        
         if (transform.position.x < screenLeftEdge)
         {
             transform.position = new Vector3(screenRightEdge, transform.position.y, transform.position.z);
--- Assets/Scripts/background1.cs
+++ Assets/Scripts/background1.cs
@@ -1,24 +1,26 @@
 using UnityEngine;
 
-public class MoveLeftAndLoop : MonoBehaviour
+public class background1 : MonoBehaviour
 {
-    public float speed = 2.0f; 
-    public float screenRightEdge = 9.0f; 
-    public float screenLeftEdge = -9.0f; 
+    public float speed = 2.0f; // 이동 속도
+    public float screenRightEdge = 17.6f; // 화면 오른쪽 경계
+    public float screenLeftEdge = -17.6f; // 화면 왼쪽 경계
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-        transform.position = new Vector3(17.6, 0, transform.position.z);
+        // 시작 위치와 크기 설정
+        transform.position = new Vector3(17.6f, 0, transform.position.z);
         transform.localScale = new Vector3(0.75f, 0.6f, transform.localScale.z);
     }
 
+    // Update is called once per frame
     void Update()
     {
-        
+        // 왼쪽으로 이동
         transform.position += Vector3.left * speed * Time.deltaTime;
 
-        
+        // 화면 왼쪽 경계를 벗어났을 때 오른쪽으로 이동
         if (transform.position.x < screenLeftEdge)
         {
             transform.position = new Vector3(screenRightEdge, transform.position.y, transform.position.z);
